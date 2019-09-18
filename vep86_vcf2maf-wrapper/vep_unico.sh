d3279b70ab42a60b20b5cdaf84b28adacc6d0809
pitta <pietro.mandreoli@gmail.com>
2019-09-18 13:46:46 +0200

    resolution_change_variable

--- vep86_vcf2maf-wrapper/vep_unico.sh
+++ vep86_vcf2maf-wrapper/vep_unico.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cmd="perl \$CONDA_DEFAULT_ENV/bin/variant_effect_predictor.pl --offline --dir \$CONDA_DEFAULT_ENV/vep_cache --force_overwrite --species $2 $3 $4 --buffer_size $5 --input_file $6 --output_file $7 --stats_file stat.htm $8"
+cmd="perl \$CONDA_PREFIX/bin/variant_effect_predictor.pl --offline --dir \$CONDA_PREFIX/vep_cache --force_overwrite --species $2 $3 $4 --buffer_size $5 --input_file $6 --output_file $7 --stats_file stat.htm $8"
 
 if [ "$1" == 1 ]; then cmd="$cmd --fork \${GALAXY_SLOTS:-4}"; fi
 
