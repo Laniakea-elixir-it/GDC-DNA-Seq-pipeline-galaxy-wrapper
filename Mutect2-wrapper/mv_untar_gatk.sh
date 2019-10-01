a877eadbd7c83d9665cee325439f713cc3cb1f19
pitta <pietro.mandreoli@gmail.com>
2019-10-01 14:29:22 +0200

    fix_shell_problem

--- Mutect2-wrapper/mv_untar_gatk.sh
+++ Mutect2-wrapper/mv_untar_gatk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #if the .jar file is not present in the conda_prefix the script search the tar.gz in the conda_prefix of the vm
 #and untar the archive
-if [[ ! -f $CONDA_PREFIX/../../GenomeAnalysisTK.jar ]] ; then
+if [ ! -f $CONDA_PREFIX/../../GenomeAnalysisTK.jar ] ; then
         tar -jxvf $CONDA_PREFIX/../../GenomeAnalysis*.tar.bz2 --strip-components=1 -C $CONDA_PREFIX/../../ 2> log_tar
 
 else
